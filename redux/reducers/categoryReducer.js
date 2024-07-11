7938035ae4b59fa9550bc4f5691a90a3fdd66558
Ali Gurur Yaman <aligururyaman@gmail.com>
2024-07-11 14:26:45 +0300

    V.0.1

--- redux/reducers/categoryReducer.js
+++ redux/reducers/categoryReducer.js
@@ -6,7 +6,7 @@ export const fetchCategories = createAsyncThunk(
   "categories/fetchCategories",
   async (_, { rejectWithValue }) => {
     try {
-      const response = await axios.get(`${mainUrl}/api/categoriesRoute`);
+      const response = await axios.get("/api/categoriesRoute");
       return response.data;
     } catch (error) {
       return rejectWithValue(error.message);
