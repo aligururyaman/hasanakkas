20ac4d3e7c60e0e5ccfa561048560600f0779950
Ali Gurur Yaman <aligururyaman@gmail.com>
2024-07-31 10:18:15 +0300

    v0.1

--- app/api/categoryRoute/route.js
+++ app/api/categoryRoute/route.js
@@ -1,12 +1,12 @@
-// pages/api/submit.js
-export async function POST(req, res) {
-  const ali = await req.json();
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
 
-  console.log(ali);
-
-  return Response.json({ data: "sdfsf" });
-}
-
-export async function GET() {
-  return Response.json({ data: "ali" });
+export async function GET(request) {
+  try {
+    const result = await sql`SELECT * FROM Pets;`;
+    return NextResponse.json(result.rows, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching pets:", error);
+    return NextResponse.json({ error: "Error fetching pets" }, { status: 500 });
+  }
 }
